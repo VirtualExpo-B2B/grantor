5f8b95e5623e84fee08d0d63b038c5f71996140a
Thomas Lecomte <th.lecomte@gmail.com>
2017-11-28 16:22:44 +0100

    added some shit for password retrieval, but commented it out cause it's probably not the right place to do it. also reworked some logv() calls and added the quick_write() function

--- gen-perms-from-mysql.py
+++ gen-perms-from-mysql.py
@@ -6,19 +6,28 @@ import time
 import os, sys
 import pymysql
 
+#
+# filesystem layout:
+#
 # - dbtype [arg]
 #   `- user [select]
 #      `- db [select]
 #      |  `- table -> perms [select]
 #      `- sources [select]
+#
 
-def list_user_perms(d, cur, user):
+def quick_write(path, contents):
+  of = open(path, 'w')
+  of.write(contents)
+  of.close()
+
+def do_user_table_privs(d, cur, user):
   '''iterates over all permissions listed for a specific user in mysql.tables_priv'''
   safe_mkdir(d)
 
   # get the sources
   # FIXME: store everything in an array THEN flush to disk to avoid partial files?
-  of = open(d + '/sources', 'w')
+  of = open(d + '/_sources', 'w')
   cur.execute("SELECT DISTINCT Host FROM mysql.tables_priv WHERE User = '%s'" % user)
   for host in cur.fetchall():
     of.write("%s\n" % host)
@@ -32,30 +41,34 @@ def list_user_perms(d, cur, user):
   cur.execute("SELECT * FROM mysql.tables_priv WHERE User = '%s' AND Host = '%s'" % (user, h))
   for host, db, user, table_name, grantor, ts,  table_priv, column_priv in cur.fetchall():
     safe_mkdir(d + '/' + db)
-    of = open(d + '/' + db + '/' + table_name, 'w')
-    of.write(table_priv)
-    of.close()
+    quick_write(d + '/' + db + '/' + table_name, table_priv)
     
-
-def list_users(d, cur):
+def do_users(d, conn):
   '''iterates over all users found in the mysql.tables_priv table'''
   logv("listing users...")
+
+  cur = conn.cursor()
+  cur_u = conn.cursor()
+
   cur.execute('SELECT DISTINCT User FROM mysql.tables_priv ORDER BY User')
   for user in cur.fetchall():
     logv("working on user %s" % user)
     du = d + '/' + user[0]
-    list_user_perms(du, cur, user[0])
+    safe_mkdir(du)
+
+    #cur_u.execute("SELECT Password FROM mysql.user WHERE User='%s'" % user)
+    #for p in cur_u.fetchall():
+    #  quick_write(du + '/_password', p[0])
+      
+    do_user_table_privs(du, cur, user[0])
 
   
 def safe_mkdir(d):
   if not os.path.isdir(d):
-    logv("creating directory %s" % d)
     try: os.mkdir(d)
     except OSError as e:
       print ( "unable to create destdir %s: %s" % ( e.filename, e.strerror ) )
       sys.exit(1)
-  else:
-    logv("directory %s already exists" % d)
 
 def logv(str):
   '''verbose logging using global crap'''
@@ -81,14 +94,13 @@ def main():
   logv("connecting to %s" % args.server[0])
 
   conn = pymysql.connect( host=args.server[0], user=args.user[0], passwd=args.passwd[0] )
-  cur = conn.cursor()
 
   logv("connected to %s" % args.server[0])
 
   for d in args.destdir[0], args.destdir[0] + '/' + args.dbtype[0]:
     safe_mkdir(d)
 
-  list_users(args.destdir[0] + '/' + args.dbtype[0], cur)
+  do_users(args.destdir[0] + '/' + args.dbtype[0], conn)
 
   conn.close()
 
