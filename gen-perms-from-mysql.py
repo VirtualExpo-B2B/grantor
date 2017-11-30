318cb471d2ac8cb1b81ecddd3a8e3786603de223
Thomas Lecomte <th.lecomte@gmail.com>
2017-11-30 13:13:54 +0100

    fixes!

--- gen-perms-from-mysql.py
+++ gen-perms-from-mysql.py
@@ -120,8 +120,9 @@ def lookup_reverse_map(h):
         "10.80.35.%": "ssolx",
         "10.80.36.%": "rplx",
         "10.80.41.%": "admlx",
-        "10.80.50.%": "applx",
+        "10.80.50.%": "applx", # bilx
         "10.80.0.0/255.255.0.0": "veso",
+        "10.80.%.%": "veso",
         "127.0.0.1":  "localhost",
         "172.16.10.%": "net-priv",
         "172.16.11.%": "net-adm",
@@ -131,7 +132,19 @@ def lookup_reverse_map(h):
         "172.16.40.%":  "velo40",
         "172.16.50.%":  "velo50",
         "::1":          "localhost",
-        "localhost":    "localhost"
+        "localhost":    "localhost",
+        "172.16.130.%": "folx",
+        "172.16.131.%": "dblx",
+        "172.16.132.%": "wklx",
+        "172.16.133.%": "bolx",
+        "172.16.134.%": "ssolx",
+        "172.16.135.%": "rplx",
+        "172.16.140.%": "folx",
+        "172.16.141.%": "dblx",
+        "172.16.142.%": "wklx",
+        "172.16.143.%": "bolx",
+        "172.16.144.%": "ssolx",
+        "172.16.145.%": "rplx",
       }
 
   if h in l:
@@ -150,30 +163,32 @@ def loop_users(d, conn):
   # we assume users from EVERY sources have the same permissions
   cur.execute('SELECT DISTINCT User FROM mysql.user ORDER BY User')
   for user in cur.fetchall():
+    user=user[0]
+    if user == '':
+      logv("warning: invalid empty username, skipping.")
+      continue
     logv("working on user %s" % user)
-    du = d + '/' + user[0]
+    du = d + '/' + user
     safe_mkdir(du)
 
-    cur.execute('SELECT Host FROM mysql.user WHERE User="%s"' % ( user ) )
-    res = cur.fetchall()
-    safe_mkdir(du + '/hosts')
-    f = open(du + '/hosts/' + args.envtype[0], 'w')
-    for host in res:
-      h = host[0]
-      meta_host = lookup_reverse_map(h)
-      if meta_host != False:
-        f.write(meta_host + "\n")
-      else:
-        print("WARNING: unknown mapping for %s\n" % (h))
-    f.close()
-
-    #cur_u.execute("SELECT Password FROM mysql.user WHERE User='%s'" % user)
-    #for p in cur_u.fetchall():
-    #  quick_write(du + '/_password', p[0])
     if args.passwords:
-      do_user_password(du, conn, user[0])
+      do_user_password(du, conn, user)
     else:
-      do_user(du, conn, user[0])
+      cur.execute('SELECT Host FROM mysql.user WHERE User="%s"' % ( user ) )
+      res = cur.fetchall()
+      safe_mkdir(du + '/hosts')
+      f = open(du + '/hosts/' + args.envtype[0], 'w')
+      for host in res:
+        h = host[0]
+        meta_host = lookup_reverse_map(h)
+        if meta_host != False:
+          f.write(meta_host + "\n")
+        else:
+          print("warning: unknown mapping %s [user=%s]" % ( h, user ))
+          if user != "root":
+            f.write(h + "\n")
+      f.close()
+      do_user(du, conn, user)
 
   
 def safe_mkdir(d):
