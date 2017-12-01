0ca3fab985d47dd95cba0332f7881770f429deed
claire.wahl <claire.wahl@virtual-expo.com>
2017-12-01 08:39:17 +0100

    check empty dirs

--- helpers/loop_from_git.py
+++ helpers/loop_from_git.py
@@ -16,11 +16,14 @@ def ensure_global_perms(conn, permsdir, function, user, envtype, envid):
         global_perms_content.append(line.split(':'))
 
     meta_hostlist = quick_read(makepath(permsdir,function,user,'hosts',envtype))
-    for meta_host in meta_hostlist:
-        sql_hostlist = get_hosts_from_meta(envtype, envid, meta_host)
-        for sql_host in sql_hostlist:
-            if not check_global_perms_ok(conn, user, sql_host, global_perms_content):
-                apply_global_perms(conn, user, sql_host, global_perms_content)
+    if not meta_hostlist:
+        logv("user %s does not exist on env %s" % (user, envtype))
+    else:
+        for meta_host in meta_hostlist:
+            sql_hostlist = get_hosts_from_meta(envtype, envid, meta_host)
+            for sql_host in sql_hostlist:
+                if not check_global_perms_ok(conn, user, sql_host, global_perms_content):
+                    apply_global_perms(conn, user, sql_host, global_perms_content)
 
 def ensure_db_perms(conn, permsdir, function, user, db):
     logv("voila")
@@ -56,16 +59,19 @@ def loop_from_git(conn, permsdir, functions, envtype, envid):
             else:
                 logv("global perms OK for %s" % (user))
 
-        for db in os.listdir(makepath(permsdir, function, user, 'databases')):
-            if not os.path.isfile(makepath(permsdir, function, user, 'databases', db, 'perms')):
-                continue
-            else:
-                if not check_db_perms(conn, permsdir, function, user, db):
-                   if not dry_run:
-                       apply_global_perms(conn, permsdir, function, user)
-                   else:
-                       logv("db perms not OK for %s on %s" % (user, db))
+        if not os.path.isdir(makepath(permsdir, function, user, 'databases')):
+            continue
+        else:
+            for db in os.listdir(makepath(permsdir, function, user, 'databases')):
+                if not os.path.isfile(makepath(permsdir, function, user, 'databases', db, 'perms')):
+                    continue
                 else:
-                    logv("db perms OK for %s on %s" % (user, db))
+                    if not check_db_perms(conn, permsdir, function, user, db):
+                       if not dry_run:
+                           apply_global_perms(conn, permsdir, function, user)
+                       else:
+                           logv("db perms not OK for %s on %s" % (user, db))
+                    else:
+                        logv("db perms OK for %s on %s" % (user, db))
 
-                 # now is the time to handle per-table privs! good luck :)
+                     # now is the time to handle per-table privs! good luck :)
