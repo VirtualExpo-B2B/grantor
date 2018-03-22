53272ea972b98a91fd13cdabc59958f34a77da43
claire-w <erialc.wahl@gmail.com>
2018-03-22 12:01:46 +0100

    meta from perms during gen

--- helpers/gen_users_from_db.py
+++ helpers/gen_users_from_db.py
@@ -10,49 +10,6 @@ import pymysql
 from helpers.do_user_password import *
 from helpers.mappings import *
 
-
-def lookup_reverse_map(h):
-    l = {
-        "%": "any",
-        "10.80.30.%": "folx",
-        "10.80.31.%": "dblx",
-        "10.80.32.%": "wklx",
-        "10.80.34.%": "bolx",
-        "10.80.35.%": "ssolx",
-        "10.80.31.12": "rdblx",
-        "10.80.36.%": "rplx",
-        "10.80.41.%": "admlx",
-        "10.80.50.%": "applx", # bilx
-        "10.80.0.0/255.255.0.0": "veso",
-        "10.80.%.%": "veso",
-        "127.0.0.1":  "localhost",
-        "172.16.10.%": "net-priv",
-        "172.16.11.%": "net-adm",
-        "172.16.200.%": "net-vpn",
-        "172.16.100.%": "net-vpn",
-        "172.16.30.%":  "velo30",
-        "172.16.40.%":  "velo40",
-        "172.16.50.%":  "velo50",
-        "::1":          "localhost",
-        "localhost":    "localhost",
-        "172.16.130.%": "folx",
-        "172.16.131.%": "dblx",
-        "172.16.132.%": "wklx",
-        "172.16.133.%": "bolx",
-        "172.16.134.%": "ssolx",
-        "172.16.135.%": "rplx",
-        "172.16.140.%": "folx",
-        "172.16.141.%": "dblx",
-        "172.16.142.%": "wklx",
-        "172.16.143.%": "bolx",
-        "172.16.144.%": "ssolx",
-        "172.16.145.%": "rplx",
-    }
-
-  if h in l:
-      return l[h]
-  return False
-
 def gen_users_from_db(d, conn):
     '''iterates over all users found in the mysql.user table'''
     global args
@@ -79,12 +36,19 @@ def gen_users_from_db(d, conn):
             f = open(du + '/hosts/' + args.envtype[0], 'w')
             for host in res:
                 h = host[0]
-                meta_host = lookup_reverse_map(h)
-                if meta_host != False:
-                    f.write(meta_host + "\n")
-                else:
-                    print("warning: unknown mapping %s [user=%s]" % ( h, user ))
-                    if user != "root":
-                        f.write(h + "\n")
+                safe_mkdir(makepath(args.permsdir, '_data', 'metamap'))
+                if not os.path.isfile(makepath(args.permsdir, '_data', 'metamap', 'common')):
+                    f_meta_common = open(makepath(args.permsdir, '_data', 'metamap', 'common'), 'w')
+                    f_meta_common.write('any: %' + "\n")
+                    f_meta_common.close
+                if os.path.isfile(makepath(args.permsdir, '_data', 'metamap', args.envtype)):
+                    meta_host = get_meta_from_host(args.permsdir, envtype, h)
+                if meta_host == False:
+                    log("WARNING: creating mapping network_%s: %s" % (user, h))
+                    f_meta_envtype = open(makepath(args.permsdir, '_data', 'metamap', args.envtype), 'w')
+                    f_meta_envtype.write('network_' + user + ': ' + h)
+                    f_meta_envtype.close
+                    meta_host = get_meta_from_host(args.permsdir, envtype, h)
+                f.write(meta_host + "\n")
             f.close()
             do_user(du, conn, user)
