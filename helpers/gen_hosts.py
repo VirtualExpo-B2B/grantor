a9d42257d15043cb6509d07f5e4688af6907fb33
claire-w <erialc.wahl@gmail.com>
2018-03-23 09:06:53 +0100

    g_net_count is not global

---
+++ helpers/gen_hosts.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+#coding: utf8
+
+import argparse
+import code
+import time
+import os, sys
+import pymysql
+
+from helpers.gen_user import *
+from helpers.mappings import *
+from helpers.common import *
+
+def gen_hosts(du, cur, user, args, g_net_count):
+    '''map all the hosts associated with a mysql user to meta hosts, and write meta hosts'''
+    logv("g_net_count = %d" % g_net_count)
+    cur.execute('SELECT Host FROM mysql.user WHERE User="%s"' % ( user ) )
+    res = cur.fetchall()
+    safe_mkdir(du + '/hosts')
+    f = open(du + '/hosts/' + args.envtype[0], 'w')
+    for host in res:
+        h = host[0]
+        safe_mkdir(makepath(args.permsdir[0], '_data'))
+        safe_mkdir(makepath(args.permsdir[0], '_data', 'metamap'))
+        meta_host = None
+        if not os.path.isfile(makepath(args.permsdir[0], '_data', 'metamap', 'common')):
+            quick_write(makepath(args.permsdir[0], '_data', 'metamap', 'common'), 'any: %' + "\n")
+        if os.path.isfile(makepath(args.permsdir[0], '_data', 'metamap', args.envtype[0])):
+            meta_host = get_meta_from_host(args.permsdir[0], args.envtype[0], h)
+            logv("meta_host from existing metamap: %s" % meta_host)
+        if meta_host == None:
+            log("WARNING: creating mapping network%d: %s" % (g_net_count, h))
+            quick_write(makepath(args.permsdir[0], '_data', 'metamap', args.envtype[0]), 'network' + str(g_net_count) + ': ' + h)
+            meta_host = 'network' + str(g_net_count)
+            g_net_count = g_net_count + 1
+            logv("new meta_host: %s" % meta_host)
+
+        f.write(meta_host + "\n")
+    f.close()
+    return g_net_count
--- helpers/gen_users_from_db.py
+++ helpers/gen_users_from_db.py
@@ -8,14 +8,16 @@ import os, sys
 import pymysql
 
 from helpers.gen_user import *
+from helpers.gen_hosts import *
 from helpers.mappings import *
 from helpers.common import *
 
 def gen_users_from_db(d, conn, args):
     '''iterates over all users found in the mysql.user table'''
-    g_net_count = 1
 
     logv("listing users...")
+    g_net_count = 1
+    logv("g_net_count = %d" % g_net_count)
   
     cur = conn.cursor()
     cur_u = conn.cursor()
@@ -31,27 +33,5 @@ def gen_users_from_db(d, conn, args):
         if args.passwords:
             gen_user_password(du, conn, user)
         else:
-            cur.execute('SELECT Host FROM mysql.user WHERE User="%s"' % ( user ) )
-            res = cur.fetchall()
-            safe_mkdir(du + '/hosts')
-            f = open(du + '/hosts/' + args.envtype[0], 'w')
-            for host in res:
-                h = host[0]
-                safe_mkdir(makepath(args.permsdir[0], '_data'))
-                safe_mkdir(makepath(args.permsdir[0], '_data', 'metamap'))
-                meta_host = None
-                if not os.path.isfile(makepath(args.permsdir[0], '_data', 'metamap', 'common')):
-                    quick_write(makepath(args.permsdir[0], '_data', 'metamap', 'common'), 'any: %' + "\n")
-                if os.path.isfile(makepath(args.permsdir[0], '_data', 'metamap', args.envtype[0])):
-                    meta_host = get_meta_from_host(args.permsdir[0], args.envtype[0], h)
-                    logv("meta_host from existing metamap: %s" % meta_host)
-                if meta_host == None:
-                    log("WARNING: creating mapping network%s: %s" % (str(g_net_count), h))
-                    quick_write(makepath(args.permsdir[0], '_data', 'metamap', args.envtype[0]), 'network' + str(g_net_count) + ': ' + h)
-                    meta_host = 'network' + str(g_net_count)
-                    g_net_count = g_net_count + 1
-                    logv("new meta_host: %s" % meta_host)
-
-                f.write(meta_host + "\n")
-            f.close()
+            g_net_count = gen_hosts(du, cur, user, args, g_net_count)
             do_user(du, conn, user)
